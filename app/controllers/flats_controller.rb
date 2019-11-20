a4c145789c4acee9ae75baaeddda0626033008f0
Matlions <matthieurpeyronnet@gmail.com>
2019-11-20 21:18:01 +0100

    90 %

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -21,7 +21,7 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     if @flat.save
-      redirect_to flats_path
+      redirect_to flats_path(@flat)
     else
       render :new
     end
@@ -33,7 +33,7 @@ class FlatsController < ApplicationController
 
   def update
     if @flat.update(flat_params)
-      redirect_to flat_path(@flat)
+      redirect_to flat_path(@flat), notice: 'Flat updated with success'
     else
       render :edit
     end
@@ -41,7 +41,7 @@ class FlatsController < ApplicationController
 
   def destroy
     @flat.destroy
-    redirect to flats_path
+    redirect to flats_path, notice: 'Flat destroyed with success'
   end
 
 private
